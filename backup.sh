ee5778a8ba9c7d27d7e47b698715e8d8e90f4169
root <eitcssa@gmail.com>
2016-05-03 17:43:55 +0000

    github backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-timestamp=`date "+%Y-%m-%d"`
+timestamp=`date "+%Y-%m-%d-%H"`
 sql_gz_file="wikidb_backup-${timestamp}.sql.gz"
 xml_gz_file="wikidb_backup-${timestamp}.xml.gz"
 
@@ -13,6 +13,5 @@ echo 'Input DB password'
 # If to backup in xml way, comment out the following line and uncomment the next command.
 mysqldump -h $hostname -u $userid -p --default-character-set=$charset $dbname | gzip > $(pwd)/$sql_gz_file
 git add $(pwd)/$sql_gz_file
-git commit -m 'backup on $(timestamp) '
+git commit -m "backup on ${timestamp}"
 git push
-echo 'done'
